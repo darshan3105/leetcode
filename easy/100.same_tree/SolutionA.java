7a7926fe5da5605f134ec152d371580814f79654
Darshan <dprabhun@amazon.com>
2022-05-04 21:18:45 +0530

    Solution for https://leetcode.com/problems/same-tree/

---
+++ easy/100.same_tree/SolutionA.java
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/same-tree/
+// TC: O(n)
+// SC: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SolutionA {
+        
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSame(p, q);
+    }
+    
+    private boolean isSame(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p==null || q==null || p.val!=q.val) {
+            return false;
+        }
+        
+        if(!isSame(p.left, q.left)) {
+            return false;
+        }
+        else {
+            if(!isSame(p.right, q.right)) {
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
