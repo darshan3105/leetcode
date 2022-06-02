082d1b0897e623bdd5515332ce35dd3d660a50b6
Darshan <dprabhun@amazon.com>
2022-06-02 09:18:07 +0530

    Solution for https://leetcode.com/problems/insertion-sort-list/

---
+++ medium/147.insertion_sort_list/SolutionA.java
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/insertion-sort-list/
+// TC: O(n)
+// SC: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class SolutionA {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+        while(cur!=null) {
+            ListNode ptr = head;
+            ListNode innerPrev = null;
+            while(ptr!=cur && ptr.val<cur.val) {
+                innerPrev=ptr;
+                ptr=ptr.next;
+            }
+            
+            if(cur==ptr) {
+                prev=cur;
+                cur=cur.next;
+            }
+            else {
+                ListNode next = cur.next;
+                if(prev!=null) {
+                    prev.next=next;
+                }
+                else {
+                    prev=cur;
+                }
+                if(innerPrev==null) {
+                    head=cur;
+                }
+                else {
+                    innerPrev.next=cur;
+                }
+                cur.next=ptr;
+                cur=next; 
+            }
+            
+        }
+        return head;
+    }
+}
