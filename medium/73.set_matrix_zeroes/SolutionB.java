1554bbebf38a1bba76a612793deb8a6bdb571148
Darshan <dprabhun@amazon.com>
2022-07-27 22:53:45 +0530

    Solution for https://leetcode.com/problems/set-matrix-zeroes/

---
+++ medium/73.set_matrix_zeroes/SolutionB.java
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/set-matrix-zeroes/
+// TC: O(m*n)
+// SC: O(1)
+
+class SolutionB {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean isRow=false, isCol=false;
+        
+        for(int i=0;i<m;i++) {
+            if(matrix[i][0] == 0) {
+                isCol = true;
+            }
+        }
+        
+        for(int j=0;j<n;j++) {
+            if(matrix[0][j] == 0) {
+                isRow = true;
+            }
+        }
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                } 
+            }
+        }
+        
+        if(isRow) {
+            for(int j=0;j<n;j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(isCol) {
+            for(int i=0;i<m;i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
