2b4063309dbe0bbbc3b562a5cd03ff72db3f57e4
darshan3105 <darshanprabhune31@gmail.com>
2024-08-31 21:44:05 +0530

    Update SolutionA.java
    removed multiplier var from palindrome_number solution

--- easy/9.palindrome_number/SolutionA.java
+++ easy/9.palindrome_number/SolutionA.java
@@ -6,11 +6,10 @@ class SolutionA {
 
         int initial_num = x;
         int final_num = 0;
-        int multiplier = 10;
         while (x!=0) {
             int rem = x%10;
             x = x/10;
-            final_num = final_num * multiplier + rem;
+            final_num = final_num * 10 + rem;
         }
 
         return initial_num == final_num;
