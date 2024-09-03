00aa50fba8660bbca13e2337737a67a8384f6388
darshan3105 <darshanprabhune31@gmail.com>
2024-09-03 10:14:30 +0530

    Updated SolutionB for https://leetcode.com/problems/add-two-numbers/
    Updated SolutionB for https://leetcode.com/problems/add-two-numbers/

--- medium/2.add_two_numbers/SolutionB.java
+++ medium/2.add_two_numbers/SolutionB.java
@@ -1,7 +1,3 @@
-// https://leetcode.com/problems/add-two-numbers/
-// TC: O(max(n, m))
-// SC: O(max(n, m))
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -14,46 +10,31 @@
  */
 class SolutionB {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Listt ans = new Listt();
-        int carry = 0;
-        while(l1!=null || l2!=null) {
+        ListNode head=null, tail=null;
+        int carry=0;
+        while (l1!=null || l2!=null) {
             int val1 = l1==null ? 0 : l1.val;
             int val2 = l2==null ? 0 : l2.val;
-            int sum = val1+val2+carry;
-            ListNode node = new ListNode(sum%10);
-            carry = sum/10;
-            ans.add(node);
-            
-            if(l1!=null) {
-                l1 = l1.next;
-            }
-            
-            if(l2!=null) {
-                l2 = l2.next;
-            }
-        }
-        
-        if(carry!=0) {
-            ans.add(new ListNode(carry));
-        }
-        return ans.head;
-    }
-}
 
-class Listt {
-    ListNode head, tail;
-    
-    public Listt() {
-        head = tail = null;
-    }
-    
-    public void add(ListNode node) {
-        if(head==null) {
-            head = tail = node;
-            return;
+            int sum = val1 + val2 + carry;
+            ListNode newNode = new ListNode(sum%10);
+            carry = sum/10;
+            if (head==null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = tail.next;
+            }
+            l1 = l1==null ? null : l1.next;
+            l2 = l2==null ? null : l2.next;
         }
-        
-        tail.next = node;
-        tail = node;
+
+        if (carry!=0) {
+            ListNode newNode = new ListNode(carry);
+            tail.next = newNode;
+            tail = tail.next;
+        }
+
+        return head;
     }
 }
